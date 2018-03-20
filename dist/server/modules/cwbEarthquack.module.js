59a2c12443174daf08aa58a1de10081d2d347482
Tsai Yi Lin <andy6804tw@yahoo.com.tw>
2018-03-20 15:47:22 +0800

    comment out AQI message unit testing

---
+++ dist/server/modules/cwbEarthquack.module.js
@@ -0,0 +1,48 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _request = require('request');
+
+var _request2 = _interopRequireDefault(_request);
+
+var _cheerio = require('cheerio');
+
+var _cheerio2 = _interopRequireDefault(_cheerio);
+
+var _uploadImgur = require('../lib/uploadImgur');
+
+var _uploadImgur2 = _interopRequireDefault(_uploadImgur);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+const getImage = () => {
+  return new Promise((resolve, reject) => {
+    (0, _request2.default)({
+      url: 'http://www.cwb.gov.tw/V7/modules/MOD_EC_Home.htm', // 中央氣象局網頁
+      method: 'GET'
+    }, (error, response, body) => {
+      if (error || !body) {
+        resolve('');
+      }
+      const $ = _cheerio2.default.load(body); // 載入 body
+      const tableTr = $('.BoxTable tr'); // 爬最外層的 Table(class=BoxTable) 中的 tr
+      const tableTd = tableTr.eq(1).find('td'); // 擷取每個欄位(td)
+      const numero = tableTd.eq(0).text(); // 取得編號
+      const pageUrl = tableTd.eq(7).text().split('.')[0]; // 取得最近一次地震網頁
+      // 判斷是否全台有感地震(quake)或區域地震(local)
+      const queryString = !Number.isNaN(Number(numero)) ? `quake/${pageUrl}` : `local/${pageUrl}`;
+      const imgUrl = `https://www.cwb.gov.tw/V7/earthquake/Data/${queryString}.gif`;
+      (0, _uploadImgur2.default)(imgUrl).then(res => {
+        console.log(res);
+        resolve(res);
+      });
+    });
+  });
+};
+
+exports.default = {
+  getImage
+};
